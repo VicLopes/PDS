8a3423012c95ba839d5d8794885b212606fdbc53
VicLopes <52095588+VicLopes@users.noreply.github.com>
2019-07-01 10:39:51 -0300

    Changed wave library to scipy's wavfile
    Started working on demodulator, modulated signal, still uses hardcoded carrier frequency

--- audioprocess.py
+++ audioprocess.py
@@ -1,95 +1,51 @@
+import scipy.io.wavfile as wav
 import numpy as np
-import wave, struct, math
 import matplotlib.pyplot as plt
 import time, sys
 import tkinter as tk
-from tkinter import filedialog
+import scipy.fftpack as fourier
 from pygame import mixer
+from tkinter import filedialog
+from scipy.signal import butter, lfilter, freqz
 
-
+#Inicializando
 root = tk.Tk()
 file_path = filedialog.askopenfilename()
-adb = wave.open(file_path, 'r') # Abre arquivo do áudio
-
-def modula():
-    amsc = wave.open("/audios/amsc.wav", 'w')
-    am = wave.open("/audios/am.wav", 'w')
-    carrier = wave.open("/audios/carrier.wav", 'w')
-
-    for n in (am, amsc, carrier):
-        n.setchannels(1)
-        n.setsampwidth(2)
-        n.setframerate(44100)
-
-    for n in range(0, adb.getnframes()):
-        base = struct.unpack('h', adb.readframes(1))[0] / 32768.0
-        carrier_sample = math.cos(3000.0 * (n / 44100.0) * math.pi * 2) # Gera um carrier de 3000 Hz
-
-        signal_am = signal_amsc = base*carrier_sample
-        signal_am += carrier_sample
-        signal_am /= 2
-        
-        amsc.writeframes(struct.pack('h', signal_amsc * 32768)) # gera um am_sc pq eu quero
-        am.writeframes(struct.pack('h', signal_am * 32768)) # gera o am que queremos
-        carrier.writeframes(struct.pack('h', carrier_sample * 32768))
-
-def demodula():
-    modulated = wave.open("/audios/am.wav", 'r')
-
-    f = demod_am = wave.open("/audios/demod_am.wav", 'w')
-    f.setnchannels(1)
-    f.setsampwidth(2)
-    f.setframerate(44100)
-
-    for n in range(0, modulated.getnframes()):
-        signal = struct.unpack('h', modulated.readframes(1))[0] / 32768.0
-        signal = abs(signal)
-        demod_am.writeframes(struct.pack('h', signal * 32767))
-        
-
 mixer.init()
+
+"""
+class Filter(object):
+    def __init__(self, cutoff):
+        self.cutoff_frequency = cutoff
+    
+    def LPF(self, n):
+        time = n
+    """
+def plotting(sig, modSig, demodSig):
+    fig, ax = plt.subplots(3, 1)
+    ax[0].plot(n,sig)
+    ax[0].set_xlabel('Time')
+    ax[0].set_ylabel('Amplitude')
+    ax[1].plot(n,modSig,'g') 
+    ax[1].set_xlabel('Carrier')
+    ax[2].plot(n,demodSig, 'r')
+    ax[2].set_xlabel('Modulated Signal')
+    plt.show()
+
+# Dá play no som
 sound = mixer.Sound(file_path)
-sound.play() # Toca o som dj
+sound.play()
+time.sleep(2)
 
-modula()
-demodula()
+(freq,sig) = wav.read(file_path)
+Fs = freq
+audlength = len(sig)/freq
+n = np.arange(0, audlength, 1/Fs)
+Fc = 88000
+Ac = 3
 
-mod_am = wave.open("/audios/am.wav", 'r')
-demod_am = wave.open("/audios/demod_am.wav", 'r')
+carrier = Ac*np.cos(2*np.pi*Fc*n)
+modulatedSig = carrier*sig
+demodulatedSig = modulatedSig * Ac*np.cos(2*np.pi*3*n)
 
-signal = adb.readframes(-1)
-signal = np.fromstring(signal, 'Int16') # Transforma o sinal em um numero para ser plotado no futuro
-fs = adb.getframerate() # Obtem a taxa de amostragem do sinal
-
-#Se for Stereo
-if adb.getnchannels() == 2:
-    print('Utilize arquivos mono apenas!')
-    sys.exit(0)
-
-signaltime = np.linspace(0, len(signal)/fs, num=len(signal))
-
-fig, ax = plt.subplots(3,1)
-
-#Plota o audio original
-ax[0].plot(signaltime, signal)
-ax[0].title('Audio original')
-ax[0].set_xlabel('Tempo')
-ax[0].set_ylabel('Amplitude')
-
-#Plota o audio modulado
-signal = mod_am.readframes(-1)
-signal = np.fromstring(signal, 'Int16')
-ax[1].plot(signaltime, signal)
-ax[1].title('Audio modulado')
-ax[1].set_xlabel('Tempo')
-ax[1].set_ylabel('Amplitude')
-
-#Plota o audio demodulado
-signal = demod_am.readframes(-1)
-signal = np.fromstring(signal, 'Int16')
-ax[2].plot(signaltime, signal)
-ax[2].title('Audio demodulado')
-ax[2].set_xlabel('Tempo')
-ax[2].set_ylabel('Amplitude')
-
-plt.show()
+plotting(sig, modulatedSig, demodulatedSig)
